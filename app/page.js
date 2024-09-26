26dd4a759805e682d645b7a7725661531b6fb087
jeevaphysicist <jeevaphysicist@gmail.com>
2024-09-26 18:44:13 +0530

    code submission

--- app/page.js
+++ app/page.js
@@ -3,8 +3,8 @@ import { MdContentPaste } from "react-icons/md";
 export default function Home() {
   return (
     <section className="flex   w-[100%] h-screen flex-col items-center justify-center">
-      <MdContentPaste className="text-[40px] sm:text-[60px] md:text-[80px] lg:text-[100px]" />
-      <div className="text-center w-[100%] font-bold text-[30px] sm:text-[40px] md:text-[60px] lg:text-[80px]">Welcome to Content Management</div>
+      {/* <MdContentPaste className="text-[40px] sm:text-[60px] md:text-[80px] lg:text-[100px]" /> */}
+      <div className="text-center w-[100%] font-bold text-[30px] sm:text-[40px] md:text-[60px] lg:text-[80px]">Welcome Page</div>
     </section>
   );
 }
